6625e9871d38d36ed6e97481ec829987b70dc9ac
Dimitris Michos <132114790+dmichos@users.noreply.github.com>
2024-07-21 13:42:40 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 // JavaScript code can be added here to enhance interactivity
 console.log('Welcome to my security research site!');
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+});
